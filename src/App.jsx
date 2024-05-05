13af508fc27296bb80edd4e3947244e4f4351b9b
withLevi <zengyin.ssr@gmail.com>
2024-05-05 16:47:29 +0800

    Add home page and about page component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
-import React, { PureComponent } from 'react'
+import React, { PureComponent } from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 export class App extends PureComponent {
   render() {
     return (
-      <div>
-        <h2>App page11</h2>
+      <div className="app">
+        <div className="header">
+         <span>Header</span>
+         <div className="nav">
+          <Link to="/home">首页</Link>
+          <Link to="/about">关于</Link>
+         </div>
+          <hr />
+        </div>
+        <div className="content">
+          {/* 映射关系：path => Component */}
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+        <div className="footer">
+          Footer
+          <hr />
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react'
+
+export class Home extends PureComponent {
+  render() {
+    return (
+      <div>
+        <h1>Home Page</h1>
+      </div>
+    )
+  }
+}
+
+export default Home
