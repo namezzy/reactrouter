ec767f51e7b7fe2b1ae0af7217b4b51efaf0e97a
withLevi <zengyin.ssr@gmail.com>
2024-05-08 16:10:54 +0800

    Add page lazy and suspese function.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { HashRouter } from "react-router-dom";
@@ -7,7 +7,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <HashRouter>
+      <Suspense fallback={<div><h3>Loading.</h3></div>}>
       <App />
+      </Suspense>
     </HashRouter>
   </React.StrictMode>
 );
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Home from "../pages/Home";
-import About from "../pages/About";
-import Login from "../pages/Login";
+// import About from "../pages/About";
+// import Login from "../pages/Login";
 import NotFoundPage from "../pages/NotFundPage";
 import HomeRanking from "../pages/HomeRanking";
 import HomeRecommend from "../pages/HomeRecommend";
@@ -10,76 +10,74 @@ import HomeSongMenu from "../pages/HomeSongMenu";
 import Detail from "../pages/Detail";
 import User from "../pages/User";
 import { Navigate } from "react-router-dom";
+import React from "react";
 
-
+const About = React.lazy( () => import("../pages/About"))
+const Login = React.lazy( () => import("../pages/Login"))
 
 const routes = [
-    
-    {
-        path: "/",
-        element: <Navigate to="/home"/>
-    },
+  {
+    path: "/",
+    element: <Navigate to="/home" />,
+  },
 
-    {
+  {
+    path: "/home",
+    element: <Home />,
+    children: [
+      {
         path: "/home",
-        element: <Home/>,
-        children: [
-            {
-                path: "/home",
-                element: <Navigate  to="/home/recommend"/>
-            },
-            {
-                path: "/home/recommend",
-                element: <HomeRecommend/>
-            },
-            {
-                path: "/home/ranking",
-                element: <HomeRanking/>
-            },
-            {
-                path: "/home/songmenu",
-                element: <HomeSongMenu/>
-            }
-        ]
-    },
+        element: <Navigate to="/home/recommend" />,
+      },
+      {
+        path: "/home/recommend",
+        element: <HomeRecommend />,
+      },
+      {
+        path: "/home/ranking",
+        element: <HomeRanking />,
+      },
+      {
+        path: "/home/songmenu",
+        element: <HomeSongMenu />,
+      },
+    ],
+  },
 
-    {
-        path: "/about",
-        element: <About/>
+  {
+    path: "/about",
+    element: <About />,
+  },
 
-    },
+  {
+    path: "/login",
+    element: <Login />,
+  },
 
-    {
-        path: "/login",
-        element: <Login/>
-    },
+  {
+    path: "/category",
+    element: <Category />,
+  },
 
-    {
-        path: "/category",
-        element: <Category/>
-    },
+  {
+    path: "/order",
+    element: <Order />,
+  },
 
-    {
-        path: "/order",
-        element: <Order/>
-    },
+  {
+    path: "/detail/:id",
+    element: <Detail />,
+  },
 
-    {
-        path: "/detail/:id",
-        element: <Detail/>
-    },
+  {
+    path: "/user",
+    element: <User />,
+  },
 
-    {
-        path: "/user",
-        element: <User/>
-    },
+  {
+    path: "*",
+    element: <NotFoundPage />,
+  },
+];
 
-    {
-        path: "*",
-        element: <NotFoundPage/>
-    }
-
-]
-
-
-export default routes
+export default routes;
